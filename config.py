95171b55b932e71a5ff01bce3f3eec39471ca737
Ring <454868943@qq.com>
2020-07-15 21:53:04 -0400

    lane detection

---
+++ config.py
@@ -0,0 +1,32 @@
+from os.path import join as pjoin
+from os.path import dirname, abspath
+
+    
+class ConfigInference(object):
+    # 目录
+    PROJECT_ROOT = dirname(abspath(__file__))
+    DATA_ROOT = pjoin(PROJECT_ROOT, 'data')
+    IMAGE_ROOT = pjoin(DATA_ROOT, 'TestImage')
+    LABEL_ROOT = pjoin(DATA_ROOT, 'TestLabel')
+    OVERLAY_ROOT = pjoin(DATA_ROOT, 'TestOverlay')
+    WEIGHTS_ROOT = pjoin(PROJECT_ROOT, 'weights')
+    PRETRAINED_WEIGHTS = pjoin(WEIGHTS_ROOT, '768x256_b8_unet_resnet34', 'weights_ep_3_0.026_0.592.pth')
+    LOG_ROOT = pjoin(PROJECT_ROOT, 'logs')
+
+    # 设备
+    DEVICE = 'cuda:0'
+
+    # 网络类型
+    NET_NAME = 'unet_resnet34'
+
+    # 网络参数
+    NUM_CLASSES = 8  # 8个类别
+    # IMAGE_SIZE = (768, 256)  # 训练的图片的尺寸(h,w)
+    IMAGE_SIZE = (1024, 384)  # 训练的图片的尺寸(h,w)
+    # IMAGE_SIZE = (1536, 512)  # 训练的图片的尺寸(h,w)
+    HEIGHT_CROP_OFFSET = 690  # 在height方向上将原图裁掉的offset
+    BATCH_SIZE = 4  # 数据批次大小
+
+    # 原图的大小
+    IMAGE_SIZE_ORG = (3384, 1710)
+
